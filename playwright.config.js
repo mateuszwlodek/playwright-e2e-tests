17c6c7e5a05d249a504842f5f297b8022c2f8a80
Kohil Namdeo <kohilnamdeo@gmail.com>
2026-01-06 11:31:48 +0000

    Install Mailosaur and update config files

--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,23 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv';
+require('dotenv').config()
+//import dotenv from 'dotenv';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+//import dotenv from 'dotenv';
+//import path from 'path';
+//dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 // Read from default ".env" file.
-dotenv.config();
+//dotenv.config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: '/.tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -52,6 +53,9 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'], viewport: { width: 1700, height: 900 } },
     },
+
+    // Preserve MAILOSAURAPIKEY if it's already set (from GitHub Actions secrets)
+   //  const preservedMailosaurKey = process.env.MAILOSAURAPIKEY || process.env.MAILOSAUR_API_KEY
 
     // {
     //   name: 'firefox',
@@ -96,4 +100,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
